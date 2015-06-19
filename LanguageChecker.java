e81b968ec4fce47c3c7ca1bb7f1e9de8664d5f09
1amBulletproof <brandon.tarney@gmail.com>
2015-06-19 00:36:29 -0400

    What did you do: Fixed language 1 check
    What Works: File I/O, Language 1 check
    
    What doesn't Work: Language 2-5 check
    
    What's left to code: language 2-5 Check, javadoc comments, extra error handling, extra inputs for testing

--- LanguageChecker.java
+++ LanguageChecker.java
@@ -60,17 +60,10 @@ public class LanguageChecker {
             else if (temp == 'B') {stackB.push(temp);} 
             
         }
-        
-        if (stackA.get_Size() != 0) // check that something was received
-        {
             if (stackA.get_Size() + stackB.get_Size() == inputString.length())//confirm there were no letters other than 'A' or 'B'
             {
                 if (stackA.get_Size() == stackB.get_Size()) {l1 = true;}
-            }
-        }
-                
-        
-        
+            }   
     }
     
     private boolean is_SameSize(Stack_LL_CHAR stackA, Stack_LL_CHAR stackB)
